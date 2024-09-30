ec0d08c6662a739885187f15ea2f3155b08dfd96
javierperez2112 <jperezlosada@itba.edu.ar>
2024-09-30 12:03:17 -0300

    Ya funciona con CSV, falta que tome los datos antes

--- src/csv_plot.py
+++ src/csv_plot.py
@@ -10,18 +10,22 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 def openfile(filename: str) -> {bool, np.array}:
     try:
         file = open(filename, mode="r")
-        dataframe = pd.read_csv(file, sep='\t') # Está en TSV, alta paja cambiarlo
+        dataframe = pd.read_csv(file, sep=',')
     except:
         return (False, None)
+    # Eliminar columnas no numéricas, ONE LINER INCOMING!!!!!
+    dataframe = dataframe.apply(pd.to_numeric, errors='coerce').dropna(axis=1)
     array = dataframe.to_numpy().transpose()
+    print(dataframe)
     return (True, array)
 
 # Grafica el np.array en el tk widget
 def graphfile(array: np.array, root: tk.Widget):
     fig = plt.Figure()
     plot1 = fig.add_subplot()
-    for i in range(1,len(array)):
-        plot1.plot(array[0], array[i])
+    for i in range(0,len(array) // 2):
+        plot1.plot(array[2*i], array[2*i+1])
+    plot1.grid(True)
     graph1 = FigureCanvasTkAgg(fig, root)
     graph1.get_tk_widget().pack(fill=tk.BOTH)
     
